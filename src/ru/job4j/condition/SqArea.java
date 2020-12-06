0d691f8d5a82d148c26a955f514c95cb024e0303
Rasim <Rasim467@mail.ru>
2020-12-07 01:24:34 +0400

    9.1. String [#423371]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,11 +2,11 @@ package ru.job4j.condition;
 
 public class SqArea {
 
-    public static double square(int p, int k)
-    {
+    @SuppressWarnings("checkstyle:LocalVariableName")
+    public static double square(int p, int k) {
         double h = p / (2 * (k + 1));
-        double L = h * k;
-        return L * h;
+        double l = h * k;
+        return l * h;
     }
 
     public static void main(String[] args) {
