5dc041d672ff79bf7939ece30024f00fc45b8329
Rasim <Rasim467@mail.ru>
2020-12-07 01:21:03 +0400

    9.1. String [#423371]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,9 +4,8 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java! ";
         System.out.println(idea);
-        System.out.println(idea + "But I am a newbie.");
+        idea += "But I am a newbie.";
         int year = 2020;
         System.out.println(idea + year);
-
     }
 }
