fa67be4ab78fcc3a56bb21de6233c68b54c41f74
Rasim <Rasim467@mail.ru>
2020-12-10 07:00:01 +0400

    4.3.2. Оператор || [#423315]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -3,8 +3,7 @@ package ru.job4j.condition;
 public class Max {
 
     public static int findMax(int left, int right) {
-        int max1 = (left > right) ? left : right;
-        return max1;
+        return  (left > right) ? left : right;
     }
 
     public static void main(String[] args) {
