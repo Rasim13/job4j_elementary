a3324007cdc9b25a6f61a75849dc6cf85b7ca5d6
Rasim <Rasim467@mail.ru>
2021-01-18 20:32:31 +0400

    4. Расстояние между точками в трехмерном пространстве. [#423519]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -7,46 +7,10 @@ import org.junit.Assert;
 public class PointTest {
 
     @Test
-    public void when00to20then2() {
-        int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when02to00then2() {
-        int expected = 2;
-        int x1 = 0;
-        int y1 = 2;
-        int x2 = 0;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when00to03then2() {
-        int expected = 3;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when40to03then2() {
-        int expected = 5;
-        int x1 = 4;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when000to400then4() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(4, 0, 0);
+        double dist = a.distance3d(b);
+        Assert.assertEquals(4, dist, 0.01);
     }
 }
